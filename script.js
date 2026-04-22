e0a998899ec484f52a53232ce6d1ade5d5c46b00
dilianbstar-byte <dilian.bstar@gmail.com>
2026-04-22 19:34:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-function login() {
+// DATA LOGIN (bisa kamu ganti)
+const USERNAME = "admin";
+const PASSWORD = "1234";
+
+// Ambil element
+const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+
+// Login
+loginBtn.addEventListener("click", () => {
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
 
-    if(user === "dilian" && pass === "1314") {
-        localStorage.setItem("login", "true");  
+    if(user === USERNAME && pass === PASSWORD) {
+        localStorage.setItem("login", "true");
         showPortfolio();
     } else {
-        alert("Login gagal!");
+        alert("Username atau password salah!");
     }
-}
+});
 
+// Tampilkan portfolio
 function showPortfolio() {
     document.getElementById("loginPage").classList.add("hidden");
     document.getElementById("portfolioPage").classList.remove("hidden");
 }
 
-function logout() {
+// Logout
+logoutBtn.addEventListener("click", () => {
     localStorage.removeItem("login");
     location.reload();
-}
+});
 
 // Auto login
 if(localStorage.getItem("login") === "true") {
     showPortfolio();
 }
-localStorage.clear();
