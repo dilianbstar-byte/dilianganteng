6075fc1e4219f75f97ce4ba245e837fcf209d1dd
dilianbstar-byte <dilian.bstar@gmail.com>
2026-04-22 18:52:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
 
-    if(user === "admin" && pass === "1234") {
+    if(user === "dilian" && pass === "1314") {
         localStorage.setItem("login", "true");
         showPortfolio();
     } else {
